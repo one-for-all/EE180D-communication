b1ea3b5cd1d491376a0722b54e12f33f0784e33c
one-for-all <jianan.li@yahoo.com>
2017-12-09 12:42:17 -0800

    add tcp communication

--- communication.py
+++ communication.py
@@ -1,84 +1,53 @@
 import asyncio
 import websockets
-import socket
+import os
 
 command = ''
+loop = asyncio.get_event_loop()
 
-print("Setting up local server")
-# Set up local server for receiving inputs from parts
-HOST = ''                 # Symbolic name meaning all available interfaces
-PORT = 6666              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(5)
+# Set up local server for receiving inputs from other processes
+HOST = ''     # Symbolic name meaning all available interfaces
+PORT = 6666   # Arbitrary non-privileged port
+print("Setting up local server for receiving inputs from other "
+      "local processes")
+async def local_socket(reader, writer):
+    message = await reader.read()
+    message = message.decode()
+    addr = writer.get_extra_info('peername')
+    print("Received {}".format(message))
+    global command
+    if message == 'START':
+        command = 'start'
+    elif message == 'PAUSE':
+        command = 'pause'
+    writer.close()
+
+local_server_coroutine = asyncio.start_server(local_socket, HOST, PORT,
+                                            loop=loop)
+local_server = loop.run_until_complete(local_server_coroutine)
+print('Serving on {}'.format(local_server.sockets[0].getsockname()))
+
 
 # Set up functions for communicating with Game
-my_ip_address = '131.179.26.104'
+print("Setting up communication with Unity game")
+# A hacky way of getting wifi ip address
+my_ip_address = os.popen('configure_edison --showWiFiIP').read()[:-1]
+print("Communication ip address {}".format(my_ip_address))
 async def send_command(websocket, path):
     global command
     async for message in websocket:
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                data = data.decode("utf-8")
-                print('Received {}'.format(data))
-                if data == 'START':
-                    command = 'start'
-                elif data == 'PAUSE':
-                    command = 'pause'
-        print("Received {}".format(message))
-        await websocket.send(command)
+        if command != '':
+            await websocket.send(command)
         command = ''
 
 send_command_server = websockets.serve(send_command, my_ip_address, 8765)
+loop.run_until_complete(send_command_server)
 
-asyncio.get_event_loop().run_until_complete(send_command_server)
-asyncio.get_event_loop().run_forever()
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
 
-
-# while True:
-#     conn, addr = s.accept()
-#     with conn:
-#         print('Connected by', addr)
-#         while True:
-#             data = conn.recv(1024)
-#             if not data:
-#                 break
-#             data = data.decode("utf-8")
-#             print('Received {}'.format(data))
-#             if data == 'START':
-#                 command = 'start'
-#             elif data == 'PAUSE':
-#                 command = 'pause'
-
-
-
-# async def hello(websocket, path):
-#     name = await websocket.recv()
-#     print("< {}".format(name))
-#
-#     greeting = "Hello {}!".format(name)
-#     await websocket.send(greeting)
-#     print("> {}".format(greeting))
-
-# start_server = websockets.serve(hello, ip_address, 8765)
-#
-# async def consumer_handler(websocket, path):
-#     async for message in websocket:
-#         print(message)
-#         reply = input()
-#         await websocket.send(reply)
-#        await consumer(message)
-#
-#async def consumer(message):
-#    print(message)
-
-# consumer_server = websockets.serve(consumer_handler, ip_address, 8765)
-
-# asyncio.get_event_loop().run_until_complete(start_server)
-# asyncio.get_event_loop().run_until_complete(consumer_server)
-# asyncio.get_event_loop().run_forever()
+local_server.close()
+loop.run_until_complete(local_server.wait_closed())
+loop.close()
---
+++ tcp_communication.py
@@ -0,0 +1,73 @@
+import asyncio
+import websockets
+import os
+
+# Set up functions for communicating with Game
+import socket
+import sys
+
+GAME_HOST = "192.168.0.3"
+GAME_PORT = 5566
+game_socket = None
+
+
+def establish_socket():
+    global game_socket;
+    for res in socket.getaddrinfo(GAME_HOST, GAME_PORT, socket.AF_UNSPEC, socket.SOCK_STREAM):
+        af, socktype, proto, canonname, sa = res
+        try:
+            game_socket = socket.socket(af, socktype, proto)
+        except OSError as msg:
+            game_socket = None
+            continue
+        try:
+            game_socket.connect(sa)
+        except OSError as msg:
+            game_socket.close()
+            game_socket = None
+            continue
+        break
+    if game_socket is None:
+        print('could not open socket to game')
+        return False
+    else:
+        return True
+
+
+def send_to_game(command):
+    global game_socket
+    try:
+        game_socket.sendall(command.encode())
+    except (BrokenPipeError, AttributeError) as e:
+        if establish_socket():
+            game_socket.sendall(command.encode())
+
+# Set up local server for receiving inputs from other processes
+loop = asyncio.get_event_loop()
+HOST = ''     # Symbolic name meaning all available interfaces
+PORT = 6666   # Arbitrary non-privileged port
+print("Setting up local server for receiving inputs from other "
+      "local processes")
+async def local_socket(reader, writer):
+    message = await reader.read()
+    message = message.decode()
+    addr = writer.get_extra_info('peername')
+    print("Received {}".format(message))
+    if message != "":
+        send_to_game(message)
+    writer.close()
+
+local_server_coroutine = asyncio.start_server(local_socket, HOST, PORT,
+                                            loop=loop)
+local_server = loop.run_until_complete(local_server_coroutine)
+print('Serving on {}'.format(local_server.sockets[0].getsockname()))
+
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+local_server.close()
+loop.run_until_complete(local_server.wait_closed())
+loop.close()
